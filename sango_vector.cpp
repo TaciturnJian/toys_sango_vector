8a33d1cb331ac01268ced4d3dbf7d0613cc9557d
Sango <66769531+TaciturnJian@users.noreply.github.com>
2023-05-08 00:43:42 +0800

    Initialize

---
+++ sango_vector.cpp
@@ -0,0 +1,6 @@
+#include "sango_vector.h"
+
+namespace sango {
+	
+
+}
---
+++ sango_vector.h
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace sango {
+	//可变长数组
+	template<typename T>
+	class vector {
+	public:
+		/* 公开变量 */
+
+		/* 公开构造器 */
+		
+		//空vector
+		vector<T>();
+		
+		//先定义一个内容的构造器
+		vector<T>(T t);
+
+		//传入数组的构造器
+		vector<T>();
+
+		/* 公开析构器 */
+
+
+	/* ^^^ 公开 ^^^//vvv 私有 vvv */
+	private:
+
+
+
+
+	/* ^^^ 私有 ^^^//vvv 保护 vvv */
+	protected:
+
+
+
+	};
+}
